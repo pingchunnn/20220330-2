2cfdee20dbbf5ecd5ad29ad6c3f86a808f9c2ec7
pingchunnn <alice92729@gmail.com>
2022-03-30 16:44:49 +0800

    加入專案檔案。

---
+++ 20220330-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220330_2
+{
+    internal class Program
+    {
+        public static bool CheckPrime(int n)
+        {
+            bool is_prime = true;
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if ((double)(n / i) == (double)n / i) //判斷是否能整除
+                {
+                    is_prime = false;
+                    break;
+                }
+            }
+            return is_prime;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 100; i <= 500; i++)
+                if (CheckPrime(i))
+                    Console.Write("{0},", i);
+            Console.ReadLine();
+        }
+    }
+}
